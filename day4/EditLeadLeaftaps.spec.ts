35815eae481dd532b05b8c2f542b0a433042cf6d
balajimohanmr <49227135+balajimohanmr@users.noreply.github.com>
2023-12-06 13:01:25 +0530

    Update EditLeadLeaftaps.spec.ts

--- day4/EditLeadLeaftaps.spec.ts
+++ day4/EditLeadLeaftaps.spec.ts
@@ -3,16 +3,20 @@ test("Create a new lead",async()=>{
     const browser=await chromium.launch({headless:false,channel:'chrome'});
     const browserContext= await browser.newContext();
     const page= await browserContext.newPage();
+    //Launching leaftaps and loggins in 
     await page.goto("http://leaftaps.com/opentaps/control/main");
     await page.locator("#username").fill("Demosalesmanager");
     await page.locator("#password").fill("crmsfa");
     await page.locator(".decorativeSubmit").click();
+    //Clicking on CRM/SFA link
     await page.locator("text=CRM/SFA").click();
+    //Tapping create lead option on the left and filling the mandatory details before submitting them 
     await page.locator("#left-content-column li:has-text('Create Lead')").click();
     await page.locator("input:right-of(:text('Company Name'))").first().fill("Das & Co");
     await page.locator("#createLeadForm_firstName").fill("Leo");
     await page.locator("#createLeadForm_lastName").fill("Das");
     await page.locator("input.smallSubmit").click();
+    //Edit the details to modify the company name and updating it 
     await page.locator("a:has-text('Edit')").click();
     await page.locator("#updateLeadForm_companyName").clear();
     await page.locator("#updateLeadForm_companyName").fill("Wipro");
